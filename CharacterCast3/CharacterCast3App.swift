5450eb914f1a7e9844d731958e6be9951bb0614e
femimarket <BJTDhwzXnYx7TLwQdyhVoArwbQnNAMJj2gq5Zs5hpump@femi.market>
2026-06-21 21:42:52 +0100

    Initial Commit

---
+++ CharacterCast3/CharacterCast3App.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterCast3App.swift
+//  CharacterCast3
+//
+//  Created by u on 21/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CharacterCast3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
